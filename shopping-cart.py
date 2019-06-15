10fbf5f24db311ce77b2aa311e4d047980268551
clroberts29 <50728923+clroberts29@users.noreply.github.com>
2019-06-15 13:46:58 -0400

    Buggy code to capture inputs & start of the receipt print out
    We're capturing and beginning to validate user inputs, but it's skipping every other input.
    
    We've started the receipt to include store name, phone number, website, address, and time.

--- shopping-cart.py
+++ shopping-cart.py
@@ -2,6 +2,8 @@
 
 #from pprint import pprint
 
+import datetime
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -25,7 +27,65 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
+# print(products)
 # pprint(products)
 
 # TODO: write some Python code here to produce the desired output
+
+# Capture user inputs
+receipt_list = []
+
+id_list = [p["id"] for p in products]
+# print(id_list)
+
+
+user_input = "String"
+
+
+while user_input != "DONE":
+    
+    user_input = (input("Please input a product identifier, or type 'DONE' if there are no more items."))
+    
+    if user_input == "DONE":
+        print("THANK YOU! Your items have been registered.")
+        print(receipt_list)
+    else:
+        print("Not done yet")
+        try:
+            user_input = int(input("Please input a product identifier, or type 'DONE' if there are no more items."))
+            print("trying...")
+        except ValueError:
+            print("Error (1)! Invalid entry. Try again.")
+
+        print(type(user_input))
+
+        if user_input in id_list:
+            receipt_list.append(user_input)
+            print(receipt_list)
+        else:
+            print("Error (2)! Invalid number. Try again.")
+    
+
+#print(user_item_list[])
+
+# Validate user inputs are legitimate
+
+# Create a "done" option
+
+# Print the receipt including...
+    
+    # Name of the store
+print("------------")
+print("Charlie's Python Emporium -- Pythons and More!")
+print("------------")
+    # A grocery store phone number and/or website URL and/or address of choice
+print("Phone Number: 1-800-GET-ITEM")
+print("Website: www.CsPythonEmp.com")
+print("1 West 4th Street")
+print("New York, NY")
+print("10001")
+
+    # The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
+currentDT = datetime.datetime.now()
+print (currentDT.strftime("%Y-%m-%d %H:%M"))
+
